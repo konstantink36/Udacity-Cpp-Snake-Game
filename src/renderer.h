6f1022552898bb9114d435bb35b2bc5ab5f467bd
Konstantin Koch <76598075+konstantink36@users.noreply.github.com>
2021-03-03 20:37:17 +0100

    Create renderer.h

---
+++ src/renderer.h
@@ -0,0 +1,46 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <vector>
+#include <memory>
+#include <iostream>
+
+#include "SDL.h"
+#include "snake.h"
+
+class Renderer {
+ public:
+  Renderer(const std::size_t screen_width, const std::size_t screen_height,
+           const std::size_t grid_width, const std::size_t grid_height);
+  ~Renderer();
+
+
+//copy constructor
+  Renderer(const Renderer& other);
+
+//copy assignment
+  Renderer &operator=(const Renderer &other);
+
+//move constructor
+  Renderer(Renderer &&other);
+
+//move assignment
+  Renderer& operator=(Renderer&& other);
+
+  void Render(Snake const snake, SDL_Point const &food);
+  void UpdateWindowTitle(int score, int fps);
+
+ private:
+  //SDL_Window 
+  std::shared_ptr<SDL_Window> sdl_window = nullptr;
+
+  //SDL_Renderer 
+  std::shared_ptr<SDL_Renderer> sdl_renderer = nullptr;
+
+  const std::size_t screen_width;
+  const std::size_t screen_height;
+  const std::size_t grid_width;
+  const std::size_t grid_height;
+};
+
+#endif
