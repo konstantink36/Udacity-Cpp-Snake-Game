b2c42c8266f177da695039c403d88edda2b1f2b4
Konstantin Koch <76598075+konstantink36@users.noreply.github.com>
2021-03-09 11:45:30 +0100

    Update renderer.cpp

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -95,6 +95,7 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
   SDL_RenderClear(sdl_renderer.get());
 
   // Render food
+  std::unique_lock<std::mutex> lck1(mtx1);  //lock
   SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
@@ -102,6 +103,7 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
 
   // Render snake's body
   SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+  
   for (SDL_Point const &point : snake.body) {
     block.x = point.x * block.w;
     block.y = point.y * block.h;
@@ -111,12 +113,14 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
   // Render snake's head
   block.x = static_cast<int>(snake.head_x) * block.w;
   block.y = static_cast<int>(snake.head_y) * block.h;
+  lck1.unlock();  //unlock
   if (snake.alive) {
     SDL_SetRenderDrawColor(sdl_renderer.get(), 0x00, 0x7A, 0xCC, 0xFF);
   } else {
     SDL_SetRenderDrawColor(sdl_renderer.get(), 0xFF, 0x00, 0x00, 0xFF);
   }
   SDL_RenderFillRect(sdl_renderer.get(), &block);
+   
 
   // Update Screen
   SDL_RenderPresent(sdl_renderer.get());
