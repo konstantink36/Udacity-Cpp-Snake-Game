0d983a1b4a6567dfd98c5041245a8beab060b7db
Konstantin Koch <76598075+konstantink36@users.noreply.github.com>
2021-03-09 11:57:30 +0100

    Update controller.h

--- src/controller.h
+++ src/controller.h
@@ -2,15 +2,23 @@
 #define CONTROLLER_H
 
 #include "snake.h"
+#include <future>
+#include <mutex>
 
 class Controller {
  public:
   void HandleInput(bool &running, Snake &snake) const;
-  //static void HandleInput(bool &running, Snake &snake);
+
+  //void HandleInput(bool &running, Snake &snake, std::promise<Snake::Direction> &&prms) const;  
+  void HandleInputp(bool &running, Snake &snake, std::promise<Snake::Direction> &&prms) const;
 
  private:
   void ChangeDirection(Snake &snake, Snake::Direction input,
                        Snake::Direction opposite) const;
+
+mutable std::mutex mtx1;	
+
+
 };
 
 #endif
