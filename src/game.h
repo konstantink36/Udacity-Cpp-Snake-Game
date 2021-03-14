8fcafa1d78be9ba595def8ed269d349ed52fa918
Konstantin Koch <76598075+konstantink36@users.noreply.github.com>
2021-03-14 08:59:09 +0100

    Update game.h

--- src/game.h
+++ src/game.h
@@ -4,38 +4,37 @@
 #include <random>
 #include <future>
 #include <thread>
-#include "SDL.h"
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include "poison2.h"
 
 class Game {
  public:
-  Game(std::size_t grid_width, std::size_t grid_height);
- // void Run(Controller const &controller, Renderer &renderer,
-    void Run(Controller &controller, Renderer renderer,
+  Game(std::size_t grid_width, std::size_t grid_height, Poison &poison);
+  void Run(Controller &controller, Renderer renderer, Poison &poison,
            std::size_t target_frame_duration);
+
   int GetScore() const;
   int GetSize() const;
-  
-  //std::promise<Snake::Direction> prms;
-  //std::future<Snake::Direction> ftr; 
+  bool gameover = false;
 
  private:
   Snake snake;
   SDL_Point food;
 
+//random device for food
   std::random_device dev;
   std::mt19937 engine;
   std::uniform_int_distribution<int> random_w;
   std::uniform_int_distribution<int> random_h;
 
   int score{0};
-
   mutable std::mutex mtx_food;	
 
   void PlaceFood();
-  void Update();
+  void Update(Poison &poison);
+  bool FoodCell(int x, int y);
 
 };
 
